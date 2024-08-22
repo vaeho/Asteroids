79cb230fe92c462b383c7ebbe9cf8e77b309f5f5
vaeho <v.vxtangishvili1982@gmail.com>
2024-08-23 00:38:38 +0400

    added player, rotation and movement

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,8 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        dt = fps.tick(60) / 1000
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-
+        
 
 
 if __name__ == "__main__":
